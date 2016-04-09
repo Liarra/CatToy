04a9abe86e5c586c45fb515625ce6bf8311c8bf8
Liarra <m.e.tigra@gmail.com>
2016-04-09 13:51:07 +0000

    code to control the servo from Pi

---
+++ ToyController/executables/servo.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+
+pwm=GPIO.PWM(11,50)
+pwm.start(0)
+
+for i in range(0,12):
+    
+    pwm.ChangeDutyCycle(i)
+    time.sleep(0.3)
+
+
+GPIO.cleanup()
